cc0d754fb7ddf922e880c2b0ba32b56dfe6911e5
Viswajith2003 <viswajithviswa715@gmail.com>
2024-03-19 19:49:00 +0530

    overall set dashbord

--- src/Pages/dashboard/Dashboard.js
+++ src/Pages/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Dashboard() {
   return (
-    <div className="bg-[#121618] h-80 text-white ml-6">
+    <div className="bg-[#121618] h-[680px] text-white ml-6">
       <h1 className="">This is the dashboard</h1>
     </div>
   );
